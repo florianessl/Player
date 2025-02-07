0d3970925a9dfdd855a9ab1cdc26a28a0599791b
florianessl <github@iessl.at>
2025-02-07 17:03:38 +0100

    Minor: Defined "ExecutionType::ManiacHook" & clarified Push instances inside battle interpreter

--- src/game_interpreter_shared.h
+++ src/game_interpreter_shared.h
@@ -63,7 +63,8 @@ namespace Game_Interpreter_Shared {
 		DeathHandler = 10,
 		/* Event code was dynamically evaluated. (ManiacCallCommand) */
 		Eval,
-		DebugCall
+		DebugCall,
+		ManiacHook
 	};
 	static constexpr auto kExecutionType = lcf::makeEnumTags<ExecutionType>(
 		"Action",
